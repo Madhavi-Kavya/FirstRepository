6c9965fb1685f943c29dc34f70a2ed87430388d3
Madhavi-Kavya <https://github.com/Madhavi-Kavya/FirstRepository>
2019-08-07 09:33:32 -0500

    Third Project

---
+++ ThirdProject/src/ThirdProjectClassOne.java
@@ -0,0 +1,7 @@
+
+public class ThirdProjectClassOne {
+	public static void main(String[] arg)
+	{
+	System.out.println("This is Class One in Thired Project");
+	}
+}
---
+++ ThirdProject/src/ThirdProjectClassTwo.java
@@ -0,0 +1,8 @@
+
+public class ThirdProjectClassTwo {
+	public static void main(String[] arg)
+	{
+	System.out.println("This is Class Two in Thired Project");
+	}
+
+}
